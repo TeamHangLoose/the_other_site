719947b242019efd20eeeb28f9696e66048431f5
robyabbts <chregi.sommer@gmail.com>
2014-12-03 19:10:54 +0100

    overload UserController

---
+++ module/User/src/User/Controller/UserController.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of ExtUserController
+ *
+ * @author win7
+ */
+class ExtUserController {
+    //put your code here
+}
